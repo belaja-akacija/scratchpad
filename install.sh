8a8f0f5d4c1cd56dc6d728769c8dc0a13d708b25
belaja-akacija <BA@pm.me>
2021-12-22 01:23:50 -0800

    added check for previous version's directory structure

--- install.sh
+++ install.sh
@@ -21,7 +21,45 @@ DEP_CHECK(){
 INSTALLER(){
   install -v scratchpad $HOME/bin/
   install -v s $HOME/bin/
-  exit 0
+}
+
+DIR_CHECK()
+{
+  # if there are directories that are named by months, ask which year to put them into
+  # Then create the directory of the year, if it doesn't already exist and cp those directories into that year's directory
+  # Then delete the directories in the original path
+  ## I suppose I need to create a for loop in order to ask the user for each directory, but give the choice to put all the directories in the same year
+
+  # First check to see if the directory exists in the first place, if no, just exit 0
+  MONTHS=(January February March April May June July August September October November December )
+
+  DIR="$HOME/.local/share/scratchpad"
+  if [[ -d $DIR ]]; then
+    echo "Exists"
+    cd $DIR
+    # check if there are directories with digits and with word characters
+    # if $dir contains a month ask which year to put that month in, TODO if $cascade=0
+    for dir in $(ls -d $DIR/*);
+    do
+      for item in ${MONTHS[@]};
+      do
+      if [[ "$dir" =~ $item ]];
+      then
+        echo -e "\n$item matched to $dir"
+        #echo "Where would you like this?"
+        read -p "Where would you like this? -- Year: " year
+        echo "cp from $dir to $HOME/.local/share/scratchpad/$year"
+        cp -v -R "$dir" "$HOME/.local/share/scratchpad/$year"
+       echo "removing $dir"
+       rm -rf "$dir"
+      else
+        2>/dev/null
+        fi
+      done
+    done
+  else
+    exit 0
+  fi
 }
 
 INIT(){
@@ -31,5 +69,7 @@ INIT(){
     mkdir $HOME/bin
   fi
   DEP_CHECK
+  DIR_CHECK
 }
 INIT
+exit 0
